9f37921da18df3914806223865fa6c0e7520702e
Robert Scholes Lyck <roschly@gmail.com>
2015-06-08 15:55:15 +0200

    Small stuff

--- src/main.c
+++ src/main.c
@@ -5,12 +5,17 @@
 * Example on how to turn on the power LED on the
 * CM510 (Atmega2561)
 */
+
 int main() {
-	DDRC = 0x11; //Set PC0 as output (others as input)
+	DDRC = 0x01; //Set PC0 as output (others as input)
+	DDRC = 0x21; //Set PC2 as output (others as input)
 	while(1) {
 		PORTC = 0x00; //Set PC0 to low (turn on led)
+		PORTC = 0x20; //Set PC2 to low (turn on led)
 		_delay_ms(100); //wait 1000 ms = 1 sec
+
 		PORTC = 0x01; //Set PC0 to high (turn off led)
+		PORTC = 0x21; //Set PC2 to high (turn off led)
 		_delay_ms(100); //wait 1000 ms = 1 sec
 	}
 	return 0;
