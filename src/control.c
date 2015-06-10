0b68a5e63b15ae6a2071631f60f2583bf81ec64a
Jeppe Enslev <jev@umwelt.dk>
2015-06-10 19:39:11 +0200

    Asds

---
+++ src/control.c
@@ -0,0 +1,110 @@
+void init() {
+	DDRA  = 0xFC;
+	PORTA = 0xFC;
+
+	serial_initialize(57600);
+	dxl_initialize( 0, DEFAULT_BAUDNUM ); // Not using device index
+	sei();	// Interrupt Enable	
+
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);	// ADC Enable, Clock 1/64div.
+}
+
+int deg2speed(int deg) {
+	return deg/100*MAX_SPEED;
+}
+
+int calcSpeed(int ir[], int baseSpeed) {
+	int speed;
+
+	if (ir[1] > 50) {
+		speed = -ir[1];
+	} else {
+		speed = baseSpeed-ir[0];
+		if (speed < 200) 
+			speed = 200;
+	}
+	return speed;
+}
+
+void setRightSpeed(int speed) {
+	int id = 8;
+
+	if (speed > 1023)
+		speed = 1023;
+
+	if (speed < -1023)
+		speed = -1023;
+
+	if (speed < 0) {
+		speed = -1*speed;
+	} else {
+		speed += 1024;
+	}
+	dxl_write_word( id , 32, speed);	
+
+}
+
+void setLeftSpeed(int speed) {
+	int id = 9;
+
+	if (speed > 1023)
+		speed = 1023;
+
+	if (speed < -1023)
+		speed = -1023;
+
+	if (speed < 0) {
+		speed = -1*speed+1024;
+	}
+	dxl_write_word( id , 32, speed);	
+
+}
+
+int getIR(int port) {
+	ADMUX = port;										// ADC Port 1 Select	
+	
+	IR_LED(port, 1);
+
+	_delay_us(12);				// Short Delay for rising sensor signal
+	ADCSRA |= (1 << ADIF);		// AD-Conversion Interrupt Flag Clear
+	ADCSRA |= (1 << ADSC);		// AD-Conversion Start
+		
+	while( !(ADCSRA & (1 << ADIF)) );	// Wait until AD-Conversion complete
+	
+	IR_LED(port, 0);
+
+	return ADC;
+}
+
+void turnLeft(int speed, int deg) {
+	setRightSpeed(deg2speed(speed)+(deg/100)*speed/2);
+	setLeftSpeed(deg2speed(speed)-(deg/100)*speed/2);
+}
+
+void turnRight(int speedDeg, int turn) {
+	int speed = deg2speed(speedDeg);
+
+	setRightSpeed(speed+(turn/100)*speed/2);
+	setLeftSpeed(speed-(turn/100)*speed/2);
+
+	printf("%i\n", speed);
+}
+
+void moveForward(int deg) {
+	setRightSpeed(deg2speed(deg));
+	setLeftSpeed(deg2speed(deg));
+}
+
+void moveBackward(int deg) {
+	setRightSpeed(deg2speed(-deg));
+	setLeftSpeed(deg2speed(-deg));
+}
+
+void bla() {
+
+
+	// int  k = 70;
+	// setRightSpeed((k - sl)*6);
+	// setLeftSpeed((k - sr)*6);
+}
+
--- src/main.c
+++ src/main.c
@@ -8,73 +8,46 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <util/delay.h>
+#include <math.h>
 #include "dynamixel.h"
 #include "serial.h"
 #include "defines.c"
-
-void setRightSpeed(int speed) {
-	int id = 8;
-	if (speed < 0) {
-		speed = -1*speed;
-	} else {
-		speed += 1024;
-	}
-	dxl_write_word( id , 32, speed);	
-
-}
-
-void setLeftSpeed(int speed) {
-	int id = 9;
-	if (speed < 0) {
-		speed = -1*speed+1024;
-	}
-	dxl_write_word( id , 32, speed);	
-
-}
-
-int getIR(int port) {
-	ADMUX = port;										// ADC Port 1 Select	
-	
-	IR_LED(port, 1);
-
-	_delay_us(12);				// Short Delay for rising sensor signal
-	ADCSRA |= (1 << ADIF);		// AD-Conversion Interrupt Flag Clear
-	ADCSRA |= (1 << ADSC);		// AD-Conversion Start
-		
-	while( !(ADCSRA & (1 << ADIF)) );	// Wait until AD-Conversion complete
-	
-	IR_LED(port, 0);
-
-	return ADC;
-}
-
+#include "control.c"
 
 int main(void)
 {
 
-	DDRA  = 0xFC;
-	PORTA = 0xFC;
-
-	serial_initialize(57600);
-	dxl_initialize( 0, DEFAULT_BAUDNUM ); // Not using device index
-	sei();	// Interrupt Enable	
-
-	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);	// ADC Enable, Clock 1/64div.
-
-	// int speed = 0;
-
-	// dxl_write_word( 8, 32, speed+1024 );	
-	// dxl_write_word( 9, 32, speed );				
-
+	init();
 
 	while(1) {
 
-		int IR[3];
-		IR[0] = getIR(1);
-		IR[1] = getIR(5);
-		IR[2] = getIR(2);
+		int ir[4];
+		ir[0] = getIR(5); //Long range front
+		ir[1] = getIR(2); //Short range front
+		ir[2] = getIR(1); //Left
+		ir[3] = getIR(6); //Right
 
-		printf("1: %i - 2: %i - 3: %i \n", IR[0], IR[1], IR[2]);
+		// int speed = calcSpeed(ir, 600);
+		int sl = ir[2];
+		int sr = ir[3];
+
+		int speed = calcSpeed(ir, 500);
+
+		// if (sl > sr && max(sl,sr)>10) {
+		// 	turnRight(speed);
+		// } else if (sl <= sr && max(sl,sr)>10) {
+ 	// 		turnLeft(speed);
+		// }
+		
+		turnRight(80, 100);	
+
+		// int k = 70;
+		// int sl = ((ir[2]+1)*k)/430;  
+		// int sr = ((ir[3]+1)*k)/350;
+
+		// printf("%i : %i \n", ir[2], ir[3]);
+		// printf("%i : %i \n", sl, sr);
+		printf("\n");
 
 		_delay_ms(500);
 	}
